d18e3b273a238f173b62768b3c02031bfcde0dc4
Alex Junior Kuss <alex.kuss@havan.com.br>
2022-05-05 10:28:47 -0300

    primeiros passos otimização

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,6 @@
-import { MovieCard } from "./MovieCard";
+import { memo } from "react";
+import { Header } from "./Header";
+import MovieCard from "./MovieCard";
 
 interface ContentProps {
   selectedGenre: {
@@ -19,20 +21,28 @@ interface ContentProps {
   }>;
 }
 
-export function Content({ selectedGenre, movies }: ContentProps) {
+function ContentComponent({ selectedGenre, movies }: ContentProps) {
   return (
     <div className="container">
-      <header>
-        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-      </header>
+      <Header selectedGenreTitle={selectedGenre.title} />
 
       <main>
         <div className="movies-list">
           {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
           ))}
         </div>
       </main>
     </div>
   )
 }
+
+export const Content = memo(ContentComponent, (prevProps, nextProps) => {
+  return prevProps.selectedGenre.id == nextProps.selectedGenre.id
+});
